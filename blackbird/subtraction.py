f375fa3a13a3f1a0f43115608fbba9f0cedb117c
thonic <a.hoskovec@gmail.com>
2022-03-25 17:01:42 +0100

    correct first squeeze

--- blackbird/subtraction.py
+++ blackbird/subtraction.py
@@ -5,7 +5,7 @@ from strawberryfields.ops import *
 prog = sf.Program(2)
 
 with prog.context as q:
-    Squeezed(1, np.pi) | q[0]
+    Squeezed(1, 0) | q[0]
     BSgate(0.1, 0) | (q[0], q[1])
     MeasureFock(select=1) | q[1]
     Squeezed(1, np.pi) | q[0]
@@ -16,4 +16,14 @@ state = result.state
 print(state.trace())
 print(np.diagonal(state.reduced_dm([0])))
 
-# result: [0.64805427 + 0.j, 0. + 0.j, 0.18794405 + 0.j, 0. + 0.j, 0.08175928 + 0.j, 0. + 0.j, 0.03951873 + 0.j, 0. + 0.j, 0.02005664 + 0.j, 0. + 0.j]
+# result:
+# [0.64805427+0.j,
+# 0.        +0.j,
+# 0.18794405+0.j,
+# 0.        +0.j,
+# 0.08175928+0.j,
+# 0.        +0.j,
+# 0.03951873+0.j,
+# 0.        +0.j,
+# 0.02005664+0.j,
+# 0.        +0.j]
