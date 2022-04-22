3574ebeefc62526141df0d79aa2c0f004fc1b485
thonic <a.hoskovec@gmail.com>
2022-04-22 11:16:18 +0200

    PRA 2005 eq. 4

--- blackbird/subtraction.py
+++ blackbird/subtraction.py
@@ -6,24 +6,18 @@ prog = sf.Program(2)
 
 with prog.context as q:
     Squeezed(1, 0) | q[0]
-    BSgate(0.1, 0) | (q[0], q[1])
+    Dgate(1) | q[0]
+    BSgate(np.pi / 4, 0) | (q[0], q[1])
     MeasureFock(select=1) | q[1]
-    Squeezed(1, np.pi) | q[0]
+    Dgate(1) | q[0]
+    Squeezed(-1, np.pi) | q[0]
 
-eng = sf.Engine("fock", backend_options={"cutoff_dim": 10})
+eng = sf.Engine("fock", backend_options={"cutoff_dim": 50})
 result = eng.run(prog)
 state = result.state
 print(state.trace())
-print(np.diagonal(state.reduced_dm([0])))
+print(np.diagonal(state.reduced_dm([0]))[:4])
 
-# result:
-# [0.64805427+0.j,
-# 0.        +0.j,
-# 0.18794405+0.j,
-# 0.        +0.j,
-# 0.08175928+0.j,
-# 0.        +0.j,
-# 0.03951873+0.j,
-# 0.        +0.j,
-# 0.02005664+0.j,
-# 0.        +0.j]
+# # result:
+# 0.9999997939494688
+# [0.64805427+0.j 0.        +0.j 0.18794405+0.j 0.        +0.j]
