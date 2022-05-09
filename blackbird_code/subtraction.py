2296e6cafe57e959a2f9bed53cc00954674d7418
Craig Hamilton <craighamilton18@gmail.com>
2022-05-09 19:46:53 +0200

    RUN 03

--- blackbird_code/subtraction.py
+++ blackbird_code/subtraction.py
@@ -12,12 +12,12 @@ def state_generation():
         # S2gate(1.1,0)| (q[0], q[1]
         Dgate(1.4144,0) | q[0]
         BSgate(0.001, 0) | (q[0], q[1])
-      #  MeasureFock(select=1) | q[1]
+        MeasureFock(select=1) | q[1]
         Dgate(1.4144,np.pi) | q[0]
         BSgate(0.001, 0) | (q[0], q[2])
-       # MeasureFock(select=1) | q[2]
+        MeasureFock(select=1) | q[2]
         Sgate(1, np.pi) | q[0]
-        MeasureFock(select=1) | q[1]
+        #MeasureFock(select=1) | q[1]
 
 
     eng = sf.Engine("fock", backend_options={"cutoff_dim": 10})
