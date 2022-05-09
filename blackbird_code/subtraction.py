5f6c7aa000cf4ef567079dea68d4cc2381d2d5a3
Craig Hamilton <craighamilton18@gmail.com>
2022-05-09 19:48:55 +0200

    RUN 04

--- blackbird_code/subtraction.py
+++ blackbird_code/subtraction.py
@@ -7,7 +7,7 @@ def state_generation():
     prog = sf.Program(3)
 
     with prog.context as q:
-        Squeezed(1, 0) | q[0]
+        Squeezed(0.8814, 0) | q[0]
        # S2gate(1,0)| (q[0], q[1])
         # S2gate(1.1,0)| (q[0], q[1]
         Dgate(1.4144,0) | q[0]
@@ -16,11 +16,11 @@ def state_generation():
         Dgate(1.4144,np.pi) | q[0]
         BSgate(0.001, 0) | (q[0], q[2])
         MeasureFock(select=1) | q[2]
-        Sgate(1, np.pi) | q[0]
+        Sgate(0.8814, np.pi) | q[0]
         #MeasureFock(select=1) | q[1]
 
 
-    eng = sf.Engine("fock", backend_options={"cutoff_dim": 10})
+    eng = sf.Engine("fock", backend_options={"cutoff_dim": 20})
     result = eng.run(prog)
     state = result.state
     return np.array([state.trace(), ]), np.diagonal(state.reduced_dm([0]))[:6]
