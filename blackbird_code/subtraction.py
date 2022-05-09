6ca3b976aaa29180a6fe7efc8fc0ba46992152e8
Craig Hamilton <craighamilton18@gmail.com>
2022-05-09 19:36:34 +0200

    RUN 02

--- blackbird_code/subtraction.py
+++ blackbird_code/subtraction.py
@@ -4,7 +4,7 @@ from strawberryfields.ops import *
 
 
 def state_generation():
-    prog = sf.Program(2)
+    prog = sf.Program(3)
 
     with prog.context as q:
         Squeezed(1, 0) | q[0]
@@ -17,7 +17,7 @@ def state_generation():
         BSgate(0.001, 0) | (q[0], q[2])
        # MeasureFock(select=1) | q[2]
         Sgate(1, np.pi) | q[0]
-       MeasureFock(select=1) | q[1]
+        MeasureFock(select=1) | q[1]
 
 
     eng = sf.Engine("fock", backend_options={"cutoff_dim": 50})
