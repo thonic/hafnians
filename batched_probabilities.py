8bcdeec57e475e7e653b2699d8c8749ea8d774a3
meet4444 <meet.panchal@alumni.iitgn.ac.in>
2023-02-18 11:47:56 +0100

    Batched_Hafnian and Photon Statistics

--- batched_probabilities.py
+++ batched_probabilities.py
@@ -1,38 +1,69 @@
 # inputs: covariance, displacement, which photons to measure
-
-import argparse
-from datetime import datetime
-
+import matplotlib.pyplot as plt
 import numpy as np
-from thewalrus.quantum import Amat, Qmat, reduced_gaussian, density_matrix_element, density_matrix
-from thewalrus.samples import hafnian_sample_state, generate_hafnian_sample
-from thewalrus import hafnian, loop_hafnian, hafnian_batched
-from time import time
-import logging
-from pathlib import Path
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.linalg import block_diag
+from thewalrus import hafnian, hafnian_batched, loop_hafnian
+from thewalrus.quantum import (Amat, Qmat, density_matrix,
+                               density_matrix_element, reduced_gaussian)
+from thewalrus.samples import generate_hafnian_sample, hafnian_sample_state
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
+r = 1.5
+sinh2r = np.sinh(2*r)
+cosh2r = np.cosh(2*r)
 
-# -----------------------------
-# Configuration
-# -----------------------------
+# Interferometer matrix
+fourier_matrix  = np.fft.fft(np.eye(2))/np.sqrt(2)
+# fourier_matrix = np.array([[1, 1j],[1j, 1]])/np.sqrt(2)
+print(fourier_matrix)
+
+fourier_conjugate = np.conjugate(fourier_matrix)
+fourier_transpose = np.transpose(fourier_matrix)
+fourier_dagger = np.transpose(fourier_conjugate)
+interferometer = block_diag(fourier_matrix, fourier_conjugate)
+interferometer_dagger = block_diag(fourier_dagger, fourier_transpose)
 
 # simple covariance matrix, two modes
-covariance_matrix = np.array([[1.5000, -1.4142], [-1.4142, 1.5000]])
+covariance_matrix = np.array([[cosh2r, 0, 0, sinh2r], [0, cosh2r, sinh2r, 0], [0, sinh2r, cosh2r, 0], [sinh2r, 0, 0, cosh2r]])
+covariance_matrix = 0.5*interferometer @ covariance_matrix @ interferometer_dagger
 
-# displacement vector for testing
-displacement_vector = np.array([1.0, 1.0])
+Q = Qmat(covariance_matrix, hbar=1)
+detq = np.linalg.det(Q)
 
-# cutoff in number of photons
-cutoff = np.array([1, 2])
-
-# -----------------------------
-# Calculation
-# -----------------------------
-
+cutoff = 3
 A = Amat(covariance_matrix, hbar=1)
 
 # see Eq. (6) in papers/boson_sampling.pdf
-hafnians = hafnian_batched(A, cutoff, mu=displacement_vector, renorm=True)
+hafnians = hafnian_batched(A, cutoff, mu=None, renorm=True)
+hafnians = hafnians/np.sqrt(detq)
 
-print(hafnians)
+# print(hafnians)
+print(hafnians[0][0][0][0])
+print(hafnians[0][1][0][1])
+print(hafnians[1][0][1][0])
+print(hafnians[1][1][1][1])
+print(hafnians[0][2][0][2])
+print(hafnians[2][0][2][0])
+print(hafnians[1][2][1][2])
+print(hafnians[2][1][2][1])
+print(hafnians[2][2][2][2])
+
+if cutoff == 2:
+    # for 0 or 1 photon present in the output mode
+    data2D = [[hafnians[0][0][0][0], hafnians[0][1][0][1]],[hafnians[1][0][1][0], hafnians[1][1][1][1]]]
+else:
+# for 0, 1 or 2 photons present in the output mode
+    data2D = [[hafnians[0][0][0][0], hafnians[0][1][0][1], hafnians[0][2][0][2]],
+              [hafnians[1][0][1][0], hafnians[1][1][1][1], hafnians[1][2][1][2]],
+              [hafnians[2][0][2][0], hafnians[2][1][2][1], hafnians[2][2][2][2]]]
+
+data_array = np.array(data2D)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+x_data, y_data = np.meshgrid( np.arange(data_array.shape[1]), np.arange(data_array.shape[0]) )
+x_data = x_data.flatten()
+y_data = y_data.flatten()
+z_data = data_array.flatten()
+ax.bar3d(x_data, y_data, np.zeros(len(z_data)), 1, 1, z_data )
+plt.show()
---
+++ photon_statistics.py
@@ -0,0 +1,83 @@
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.linalg import block_diag
+from scipy.special import genlaguerre
+
+
+def probability(n1, n2, a1, a2, r):
+    p = min(n1,n2)
+    q = max(n1,n2)
+    tanhr = np.tanh(r)
+    coshr = np.cosh(r)
+    u1 = a1 + np.conj(a2)*tanhr
+    u2 = a2 + np.conj(a1)*tanhr
+    u = (u1*u2)/tanhr
+    L = genlaguerre(p,q-p)(u)
+
+    # Amplitude C(n1,n2)
+    t = -1*tanhr
+    pf = math.factorial(p)
+    qf = math.factorial(q)
+    earg = -1*(np.conj(a1)*u1 + np.conj(a2)*u2)/2
+    exp = math.exp(earg)
+    c = (np.power(t,p)/coshr) * np.power((pf/qf),0.5) * np.power(u1,n1-p) * np.power(u2,n2-p)* L * exp
+
+    #Probability Distribution P(n,n)
+    if n1 == n2:
+        n = n1
+        absL = np.power(abs(L),2)
+        earg2 = -1*(np.power(abs(a1),2) + np.power(abs(a2),2) + tanhr*(a1*a2 + np.conj(a1)*np.conj(a2)))
+        prob = (np.power(tanhr,2*n)/coshr) * absL * math.exp(earg2)
+    #Probability Distribution P(n1,n2)
+    else:
+        prob = np.power(abs(c),2)
+    return prob
+
+#two mode squeezed state sample configuration.
+diagonal = False
+a1 = 3.0
+a2 = 3.0
+r = 1.5
+
+if diagonal:
+    # Two Dimensional n1 = n2 plot
+    x = [i for i in range(50)]
+    prob = []
+    for n in range(50):
+        n1 = n
+        n2 = n
+        prob.append(probability(n1, n2, a1, a2, r))
+    fig = plt.figure()
+    ax = plt.axes()
+    ax.plot(x, prob)
+    plt.show()
+
+else:
+    # Three Dimensional plot for different values of n1, n2
+    n1 = np.arange(0, 50, 1)
+    n2 = np.arange(0, 50, 1)
+    x, y = np.meshgrid(n1, n2)
+    prob = np.array([probability(n1,n2,a1,a2,r) for n1,n2 in zip(np.ravel(x), np.ravel(y))])
+    z = prob.reshape(x.shape)
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    ax.set_zlim(0, 0.02)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    # A StrMethodFormatter is used automatically
+    ax.zaxis.set_major_formatter('{x:.02f}')
+    plt.show()
+
+
+
+    
+
+
+
+
+
+
