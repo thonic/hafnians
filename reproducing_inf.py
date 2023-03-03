22d14cc758af7308265697b9022ebc9ff1464947
thonic <a.hoskovec@gmail.com>
2023-03-03 14:45:28 +0100

    simplest case that should diverge

---
+++ reproducing_inf.py
@@ -0,0 +1,23 @@
+# inputs: covariance, displacement, which photons to measure
+import numpy as np
+from thewalrus import hafnian_batched
+from thewalrus.quantum import Amat, Qmat
+
+# simple covariance matrix, two modes
+covariance_matrix = np.identity(4)
+mu = np.array([2] * 4)
+
+Q = Qmat(covariance_matrix, hbar=1)
+detq = np.linalg.det(Q)
+
+cutoff = 2
+A = Amat(covariance_matrix, hbar=1)
+
+hafnians = hafnian_batched(A, cutoff, mu=mu, renorm=True)
+hafnians = hafnians / np.sqrt(detq)
+
+# print(hafnians)
+print(hafnians[0][0][0][0])
+print(hafnians[0][1][0][1])
+print(hafnians[1][0][1][0])
+print(hafnians[1][1][1][1])
